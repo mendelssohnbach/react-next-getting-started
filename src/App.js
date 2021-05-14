b008b86defde693d74dac867b85c8ad742da3c2d
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-15 05:43:21 +0900

    list3-17 and 3-18

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
+import React from 'react';
 import './App.css';
-import Rect from './Rect';
 
-function App(props) {
-  return (
-    <div>
-      <h1 className="bg-primary text-white display-4">React</h1>
-      <div className="container">
-        <p className="subtitle">draw rectangle.</p>
-        <Rect x="200" y="" w="200" h="200" c="#6ff9" r="25" />
-        <Rect x="300" y="300" w="200" h="200" c="#f6f9" r="75" />
-        <Rect x="400" y="400" w="200" h="200" c="#6669" r="100" />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: 'Hello Component',
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="bg-primary text-white display-4">React</h1>
+        <div className="container">
+          <p className="subtitle">Show message</p>
+          <p className="alert alert-warning">{this.state.msg}</p>
+          <p className="alert alert-dark">{this.props.msg}</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
