a9493009b01e33dd292000ddb11dd9413c4376d3
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-16 07:38:23 +0900

    list4-3

--- src/App.js
+++ src/App.js
@@ -3,23 +3,44 @@ import './App.css';
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [flag, setFlag] = useState(false);
 
   const clickFunc = () => {
     setCount(count + 1);
+  };
+  const changeFlg = (e) => {
+    setFlag(e.target.checked);
   };
 
   return (
     <div>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
-        <h4 className="my-3">Hooks sample</h4>
-        <div className="alert alert-primary text-center">
-          <p className="h5 mb-3">{`click:${count} times!`}.</p>
-          <div>
-            <button className="btn btn-primary" onClick={clickFunc}>
-              Click me
-            </button>
+        <div className="my3">Hooks sample</div>
+        {flag ? (
+          <div class="alert alert-primary text-center">
+            <p className="h5 mb-3">click: {count} times!</p>
+            <div>
+              <button className="btn btn-primary" onClick={clickFunc}>
+                Click me
+              </button>
+            </div>
           </div>
+        ) : (
+          <div className="card p-3 border-primary text-center">
+            <p className="h5 mb-3 text-left text-primary">Click: {count} times!</p>
+            <div>
+              <button className="btn btn-primary" onClick={clickFunc}>
+                Click me
+              </button>
+            </div>
+          </div>
+        )}
+        <div className="form-group h6 text-center pt-3">
+          <input type="checkbox" className="form-check-input" id="check1" onChange={changeFlg} />
+          <label htmlFor="check1" className="form-check-label">
+            Change from style.
+          </label>
         </div>
       </div>
     </div>
