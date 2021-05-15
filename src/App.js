61f2c5ed3c52f2e150618cfff3a282ded1d6903f
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-15 15:19:50 +0900

    list3-26

--- src/App.js
+++ src/App.js
@@ -1,54 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-class App extends React.Component {
+class App extends Component {
   input = '';
 
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'input form',
+      message: 'type your name.',
+    };
+    this.doChange = this.doChange.bind(this);
+    this.doSubmit = this.doSubmit.bind(this);
+  }
+
+  doChange(event) {
+    this.input = event.target.value;
+  }
+
+  doSubmit(event) {
+    this.setState({
+      title: 'send form',
+      message: `Hello, ${this.input}!!`,
+      // message: 'Hello, ' + this.input + '!!',
+    });
+    event.preventDefault();
+  }
 
   render() {
     return (
       <div>
         <h1 className="bg-primary text-white display-4">React</h1>
         <div className="container">
-          <Message title="Children!">
-            これはコンポーネント内のコンテンツです。
-            マルでテキストを分割し、リストにして表示します。 改行は必要ありません。
-          </Message>
+          <h4>{this.state.title}</h4>
+          <p className="card h5 p-3">{this.state.message}</p>
+          <div className="alert alert-primary mt-3">
+            <form onSubmit={this.doSubmit}>
+              <div className="form-group">
+                <label>Message:</label>
+                <input type="text" className="form-control" onChange={this.doChange} />
+              </div>
+              <input type="submit" className="btn btn-primary" value="Click" />
+            </form>
+          </div>
         </div>
-      </div>
-    );
-  }
-}
-
-class Message extends React.Component {
-  li = {
-    fontSize: '14pt',
-    fontWeight: 'bold',
-    color: '#090',
-  };
-
-  render() {
-    const content = this.props.children;
-    const arr = content.split('。');
-    let arr2 = [];
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i].trim() !== '') {
-        arr2.push(arr[i]);
-      }
-    }
-    const list = arr2.map((value, key) => (
-      <li className="list-group-item" style={this.li} key={key}>
-        {key + 1}. {value} .
-      </li>
-    ));
-
-    return (
-      <div>
-        <h2>{this.props.title}</h2>
-        <ol className="list-group">{list}</ol>
       </div>
     );
   }
