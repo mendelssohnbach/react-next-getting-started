1d436ba07ad4394dbbe73253c8f7c455b8602f6b
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-15 17:15:17 +0900

    list3-31

--- src/App.js
+++ src/App.js
@@ -6,11 +6,21 @@ const data = { title: 'React-Context', message: 'this is sample message.' };
 const SampleContext = React.createContext(data);
 
 class App extends Component {
+  newdata = { title: '新しいタイトル', message: 'これは新しいメッセージです。' };
+
   render() {
     return (
       <div>
         <h1 className="bg-primary text-white display-4">React</h1>
         <div className="container">
+          <Title />
+          <Message />
+          <hr />
+          <SampleContext.Provider value={this.newdata}>
+            <Title />
+            <Message />
+          </SampleContext.Provider>
+          <hr />
           <Title />
           <Message />
         </div>
