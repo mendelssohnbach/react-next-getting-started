275f237e01a6e77a51b7d9483f25274a4f43cfe0
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-17 05:21:22 +0900

    list4-10

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import { useState, useEffect } from 'react';
-import AlertMessage from '../src/components/AlertMessage';
+// import AlertMessage from '../src/components/AlertMessage';
 import Header from './components/Header';
 import './App.css';
 
+function AlertMessage(props) {
+  return (
+    <div className="alert alert-primary h5 text-primary">
+      <h5>{props.msg}</h5>
+    </div>
+  );
+}
+
 function App() {
-  const [val, setVal] = useState(0);
-  const [msg, setMsg] = useState('set a number...');
+  const [val, setVal] = useState(1000);
+  const [tax1, setTax1] = useState(0);
+  const [tax2, setTax2] = useState(0);
+  const [msg, setMsg] = useState(<p>set a price...</p>);
 
   const doChange = (event) => {
     setVal(event.target.value);
   };
 
+  const doAction = () => {
+    let res = (
+      <div>
+        <p>軽減税率(8%) : {tax1} 円</p>
+        <p>軽減税率(10%) : {tax2} 円</p>
+      </div>
+    );
+    setMsg(res);
+  };
+
   useEffect(() => {
-    let total = 0;
-    for (let i = 0; i <= val; i++) {
-      total += 1;
-    }
-    setMsg(`total: ${total} .`);
+    setTax1(Math.floor(val * 1.08));
+  }, [val]);
+
+  useEffect(() => {
+    setTax2(Math.floor(val * 1.1));
   }, [val]);
 
   return (
@@ -25,10 +45,13 @@ function App() {
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
         <AlertMessage msg={msg} />
-        <div className="from-group">
-          <label htmlFor="Input:"></label>
+        <div className="form-group">
+          <label>Input:</label>
           <input type="number" className="form-control" onChange={doChange} />
         </div>
+        <button className="btn btn-primary" onClick={doAction}>
+          Calc
+        </button>
       </div>
     </div>
   );
