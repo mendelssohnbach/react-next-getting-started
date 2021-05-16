c0ca39dcc6e106455af554e43cba4883923a8ea6
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-16 12:12:33 +0900

    list4-8

--- src/App.js
+++ src/App.js
@@ -1,27 +1,83 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { AlertMessage } from './components/AlertMessage';
-import { CardMessage } from './components/CardMessage';
 
-const App = () => {
-  const [alert, setAlert] = useState('This is alert message!');
-  const [card, setCard] = useState('This is card message');
+function AlertMessage(props) {
+  const data = props.data;
+  const msg = JSON.stringify(data);
+
+  return (
+    <div className="alert alert-primary h5 text-primary">
+      <h5>{msg}</h5>
+      <hr />
+      <table className="table h6">
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <td>{data.name}</td>
+          </tr>
+          <tr>
+            <th>Mail</th>
+            <td>{data.mail}</td>
+          </tr>
+          <tr>
+            <th>Age</th>
+            <td>{data.age}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+function App() {
+  const [name, setName] = useState('');
+  const [mail, setMail] = useState('');
+  const [age, setAge] = useState(0);
+  const [form, setForm] = useState({
+    name: 'no name',
+    mail: 'no mail',
+    age: 0,
+  });
+
+  const doChangeName = (event) => {
+    setName(event.target.value);
+  };
+  const doChangeMail = (event) => {
+    setMail(event.target.value);
+  };
+  const doChangeAge = (event) => {
+    setAge(event.target.value);
+  };
+
+  const doSubmit = (event) => {
+    setForm({ name: name, mail: mail, age: age });
+    event.preventDefault();
+  };
 
   return (
     <div>
-      <h1 className="bg-primary text-white display-4">React</h1>
+      <h1 className="bg-primary text-white display-4 ">React</h1>
       <div className="container">
-        <div className="my-3">Hooks sample</div>
-        <AlertMessage alert={alert} setAlert={setAlert} />
-        <CardMessage card={card} setCard={setCard} />
-        <hr />
-        <div className="text-right">
-          <p>{alert}</p>
-          <p>{card}</p>
-        </div>
+        <h4 className="my-3">Hooks sample</h4>
+        <AlertMessage data={form} setData={setForm} />
+        <form onSubmit={doSubmit}>
+          <div className="form-group">
+            <label>Name:</label>
+            <input type="text" className="form-control" onChange={doChangeName} />
+          </div>
+          <div className="form-group">
+            <label>Mail:</label>
+            <input type="text" className="form-control" onChange={doChangeMail} />
+          </div>
+          <div className="form-group">
+            <label>Age:</label>
+            <input type="number" className="form-control" onChange={doChangeAge} />
+          </div>
+          <input type="submit" className="btn btn-primary" value="Click" />
+        </form>
       </div>
     </div>
   );
-};
+}
 
 export default App;
--- src/components/AlertMessage.jsx
+++ src/components/AlertMessage.jsx
@@ -1,19 +1,30 @@
 import '../App.css';
 
 export const AlertMessage = (props) => {
-  const data = ['Hello!', 'Welcome...', 'Good-bye'];
-
-  const actionAlert = () => {
-    const re = data[Math.floor(Math.random() * data.length)];
-    props.setAlert(`message: ${re}.`);
-  };
+  const data = props.data;
+  const msg = JSON.stringify(data);
+  console.log(msg);
 
   return (
     <div className="alert alert-primary h5 text-primary">
-      <h5>{props.alert}</h5>
-      <button onClick={actionAlert} className="btn btn-primary">
-        Click me!
-      </button>
+      <h5>{msg}</h5>
+      <hr />
+      <table className="table h6">
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <td>{data.name}</td>
+          </tr>
+          <tr>
+            <th>Mail</th>
+            <td>{data.mail}</td>
+          </tr>
+          <tr>
+            <th>Age</th>
+            <td>{data.age}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
